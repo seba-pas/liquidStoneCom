527ab68c2b8ed92ebacdf27f5d97619434c44276
Sebastian Pastorenzi <102265528+seba-pas@users.noreply.github.com>
2023-06-17 00:57:38 -0300

    responsive

--- src/components/grid/Grid.jsx
+++ src/components/grid/Grid.jsx
@@ -3,14 +3,14 @@ import "./grid.css";
 
 export default function Grid() {
   return (
-    <div style={{ display: "flex", flexDirection: "column", width: '80%', marginLeft: '10%', marginBottom: '100px' }}>
+    <div className='gridWrap'>
       <div className="gridContainer">
         <div className="sq1"><p className='prod'>KITS</p></div>
-        <div className="sq2"></div>
+        <div className="sq2"><p className='prod'>TUTORIALES</p></div>
       </div>
       <div className="gridContainer">
-        <div className="sq2"></div>
-        <div className="sq1"></div>
+        <div className="sq2"><p className='prod'>EXOTICOS</p></div>
+        <div className="sq1"><p className='prod'>MARMOL</p></div>
       </div>
     </div>
   );
